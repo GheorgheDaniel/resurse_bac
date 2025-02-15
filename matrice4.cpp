2676c0a710fd5410780e3d3165135de9ff18d644
GheorgheDaniel <76534772+GheorgheDaniel@users.noreply.github.com>
2025-02-15 18:22:08 +0200

    Create matrice4.cpp

---
+++ matrice4.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int M, N, a[51][51];
+
+int main() {
+
+	cin >> M >> N;
+
+	for(int i = 1; i <= M; i++) {
+		for(int j = 1; j <= N; j++) {
+			cin >> a[i][j];
+		}
+	}
+
+	for(int j = 1; j <= N; j++) // parcurgem coloanele
+	{
+
+		bool ok = 1;
+
+		//cum parcurgem liniile? 
+
+		//if(ok == 1) cout << ... 
+
+	}
+
+}
